fc01de5da5a77864bbe95d4f63ab1959baa45ba6
Celia <>
2021-05-18 20:56:01 +0000

    Made app fully functional (without database) + small updates to css

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from wtforms import StringField, TextAreaField, IntegerField, SubmitField
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mysecret'
 
-@app.route('/')
+todos = ["Learn Flask", "Clean Bathroom", "Organize toys"]
+
+class TodoForm(FlaskForm):
+  todo = StringField("To Do")
+  submit = SubmitField("Add Todo")
+
+@app.route('/', methods = ["GET", "POST"])
 def index():
-    return render_template('index.html')
-
-#app = Flask('app')
-
-#@app.route('/')
-#def hello_world():
-#  return 'Hello, World!'
+  if 'todo' in request.form:
+    todos.append(request.form['todo'])
+  return render_template('index.html', todos=todos, template_form = TodoForm())
 
 app.run(host='0.0.0.0', port=8080)
